565821db284d7218778cb71549009ab60057e9c4
Miste Ry <47310669+misteryTech@users.noreply.github.com>
2024-10-29 08:17:27 +0800

    update
    update

--- include/connection.php
+++ include/connection.php
@@ -6,9 +6,3 @@ $dbname = "crm_gfi";
 
 $connection = mysqli_connect($localhost, $username, $password, $dbname);
 
-if (!$connection) {
-    die("Connection failed: " . mysqli_connect_error());
-} else {
-    echo "Connected successfully";
-}
-?>
